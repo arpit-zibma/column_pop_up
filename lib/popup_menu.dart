94e657b0f5518d5c8a26295f9bafc8d5edbcc043
Khuntarpit <66566247+Khuntarpit@users.noreply.github.com>
2020-12-06 12:37:41 +0530

    Update popup_menu.dart

--- lib/popup_menu.dart
+++ lib/popup_menu.dart
@@ -470,25 +470,24 @@ class _MenuItemWidgetState extends State<_MenuItemWidget> {
       );
     } else {
       // only text
-      return Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-               child: DecoratedBox(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.orange.withOpacity(0.8),
-                  ),
-                    child: Center(
-                      child: Text(
-                        widget.item.menuTitle,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Colors.white),
-                      ),
+      return Container(
+           height: MediaQuery.of(context).size.height*0.05,
+           width: MediaQuery.of(context).size.width*0.1,
+             child: DecoratedBox(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.orange.withOpacity(0.8),
+                ),
+                  child: Center(
+                    child: Text(
+                      widget.item.menuTitle,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.white),
                     ),
-                    )
-    ),
-      );
+                  ),
+                  )
+    );
       //   Container(
       //   child: Center(
       //     child: Material(
