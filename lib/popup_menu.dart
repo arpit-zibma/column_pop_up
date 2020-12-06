fd937fd1fb380fe9609d188d81cd0904d763b452
Khuntarpit <66566247+Khuntarpit@users.noreply.github.com>
2020-12-06 10:11:51 +0530

    Update popup_menu.dart

--- lib/popup_menu.dart
+++ lib/popup_menu.dart
@@ -44,7 +44,7 @@ typedef MenuClickCallback = Function(MenuItemProvider item);
 typedef PopupMenuStateChanged = Function(bool isShow);
 
 class PopupMenu {
-  static var itemWidth = MediaQuery.of(context).size.height*0.08;
+  static var itemWidth = MediaQuery.of(context).size.height*0.16;
   static var itemHeight = MediaQuery.of(context).size.width*0.06;
   static var arrowHeight = 10.0;
   OverlayEntry _entry;
